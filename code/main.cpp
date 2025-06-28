f833022fa053ae367b6dc5fe8912a57f619fab3a
byter64 <byter64@gmx.de>
2025-06-28 20:28:45 +0200

    Added clearframe

--- code/main.cpp
+++ code/main.cpp
@@ -6,10 +6,11 @@ int main()
 {
 	Hall::Init();
 
-	Halib::Image hansLogo = Halib::Image("assets/hans_logo.bmp");
+	Halib::Image hansLogo("assets/hans_logo.bmp");
 	//This is your game loop. The program should never leave it.
 	while(!Hall::ShouldClose()) 
 	{
+		Halib::Misc::ClearFrame(0b01);
 		hansLogo.Draw(Halib::VecI2{40, 40});
 		Halib::Misc::ShowFrame();
 	}
