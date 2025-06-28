caaac148dd92c2db1b36fde2dc9dc93fbd239a1a
byter64 <byter64@gmx.de>
2025-06-28 09:39:14 +0200

    Added software template

---
+++ code/main.cpp
@@ -0,0 +1,25 @@
+#include <Hall/Hall.h>
+
+
+int main() 
+{
+	//This is your game loop. The program should never leave it.
+	while(true) 
+	{
+		Hall::Clear(0b0100001000000001); 	//Clear the screen to R5G5B5A1(16, 16, 0, 1) or in RGBA8: (131, 131, 0, 255)
+		while (Hall::GetIsGPUBusy());		//Wait for the GPU until it finished the last draw call
+
+
+		bool vSync = false;
+		bool newVSync = false;
+		while(!(!vSync && newVSync)) 		//Wait until vSync == false and newVsync == true
+		{									//Sidenote: This way of swapping the framebuffer vsyncs your game
+			vSync = newVSync;
+			newVSync = Hall::GetVSync();
+		}
+		
+		Hall::SetCommandSwapBuffers();		//Swap the frame buffers so that your bewritten frame is shown
+
+	}
+	return 0;
+}
