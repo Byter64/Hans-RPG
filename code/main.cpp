01fba15ecd495451007f545308ab10ea9490255b
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2025-06-28 16:36:38 +0200

    Added logo

--- code/main.cpp
+++ code/main.cpp
@@ -1,25 +1,17 @@
 #include <Hall/Hall.h>
-
+#include "Halib/Image.h"
+#include "Halib/Misc.h"
 
 int main() 
 {
+	Hall::Init();
+
+	Halib::Image hansLogo = Halib::Image("assets/hans_logo.bmp");
 	//This is your game loop. The program should never leave it.
-	while(true) 
+	while(!Hall::ShouldClose()) 
 	{
-		Hall::Clear(0b0100001000000001); 	//Clear the screen to R5G5B5A1(16, 16, 0, 1) or in RGBA8: (131, 131, 0, 255)
-		while (Hall::GetIsGPUBusy());		//Wait for the GPU until it finished the last draw call
-
-
-		bool vSync = false;
-		bool newVSync = false;
-		while(!(!vSync && newVSync)) 		//Wait until vSync == false and newVsync == true
-		{									//Sidenote: This way of swapping the framebuffer vsyncs your game
-			vSync = newVSync;
-			newVSync = Hall::GetVSync();
-		}
-		
-		Hall::SetCommandSwapBuffers();		//Swap the frame buffers so that your bewritten frame is shown
-
+		hansLogo.Draw(Halib::VecI2{40, 40});
+		Halib::Misc::ShowFrame();
 	}
 	return 0;
 }
