aad857cd14b0198937c34656461f6bd63de3dd77
byter64 <byter64@gmx.de>
2025-07-07 21:29:58 +0200

    Âdded audio

--- code/source/main.cpp
+++ code/source/main.cpp
@@ -1,5 +1,6 @@
 #include "Halib/Graphic.h"
 #include "Halib/System.h"
+#include "Halib/Audio.h"
 #include "SplashScreen.h"
 #include "PartyCharacter.h"
 
@@ -9,14 +10,15 @@ int main()
 
 	SplashScreen::ShowSplashScreen();
 
+	std::shared_ptr<Halib::Audio> music = Halib::audiosystem.LoadMusic("assets/Audio/gustygardengalaxy.wav", 114.2f);
+	Halib::audiosystem.Play(music);
+
 	Halib::Sprite playerSprite = Halib::Sprite("assets/NinjaAdventure/Actor/Characters/NinjaMasked/SpriteSheet.bmp", Halib::VecI2(4, 7));
 	playerSprite.SetupAnimation(Halib::VecI2(0, 0), 4, 3, Halib::Sprite::DOWN);
 	playerSprite.isPlaying = true;
 	std::shared_ptr<PartyCharacter> player = std::make_shared<PartyCharacter>(playerSprite, Halib::VecI2(200, 120));
 	Halib::AddEntity(player);
 
-	auto background = std::make_shared<Halib::Tilemap>("assets/NinjaAdventure/Backgrounds/Tilesets/TilesetWater.bmp", Halib::VecI2(28, 17), "assets/Tilemaps/mainMap.json");
-	Halib::rendersystem.tilemap = background;
 
 	Halib::rendersystem.backgroundColor = Halib::Color(15, 15, 15);
 
